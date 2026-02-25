dd5a9646b1a64a77a7fa14a43f5bf43956fd7776
Nilay Alwar <npalwar@tamu.edu>
2026-02-25 13:33:53 -0600

    Improved items and prices view

--- src/main/java/com/boba/ItemPriceController.java
+++ src/main/java/com/boba/ItemPriceController.java
@@ -2,23 +2,54 @@ package com.boba;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class ItemPriceController {
 
     @FXML
+    private Button addInventoryItem;
+
+    @FXML
+    private TextField itemID;
+
+    @FXML
+    private TextField itemID1;
+
+    @FXML
+    private TextField itemName;
+
+    @FXML
+    private TextField itemName1;
+
+    @FXML
+    private TextField itemPrice;
+
+    @FXML
+    private TextField itemPrice1;
+
+    @FXML
+    private TextField itemSize;
+
+    @FXML
+    private TextField itemSize1;
+
+    @FXML
+    private Button updateInventoryItem;
+
+    @FXML
+    void addInventoryItem(ActionEvent event) {
+
+    }
+
+    @FXML
     void returnHome(ActionEvent event) {
-        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
-        MainApp.switchScene(stage, "/fxml/ManagerView.fxml");
+
     }
 
     @FXML
-    void changePrice(ActionEvent event) {
-        // TODO
+    void updateInventoryItem(ActionEvent event) {
+
     }
 
-    @FXML
-    void saveChanges(ActionEvent event) {
-        // TODO
-    }
 }
