29bed43c6fcfeeb567285710314213866f91d838
jajoker500 <jajoker500@email.tamu.edu>
2026-02-24 23:32:15 -0600

    Price controller added for the manager

---
+++ src/main/java/com/boba/PriceController.java
@@ -0,0 +1,29 @@
+package com.boba;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
+public class PriceController {
+
+    @FXML
+    void returnHome(ActionEvent event) {
+        Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
+        MainApp.switchScene(stage, "/fxml/managerview.fxml");
+    }
+
+    @FXML
+    void addItem(ActionEvent event) {
+        // TODO
+    }
+
+    @FXML
+    void saveChanges(ActionEvent event) {
+        // TODO
+    }
+
+    @FXML
+    void deleteItem(ActionEvent event) {
+        // TODO
+    }
+}
