bd7c313549fab507f5552e7fb808c51ca4fa9eaa
Grant Duong <grantd729@gmail.com>
2026-02-25 18:20:27 -0600

    addItem is done

--- src/main/java/com/boba/InventoryController.java
+++ src/main/java/com/boba/InventoryController.java
@@ -1,10 +1,19 @@
 package com.boba;
-
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import io.github.cdimascio.dotenv.Dotenv;
+import java.util.List;
+import java.util.ArrayList;
+
+import java.sql.*;
 
 public class InventoryController {
 
@@ -50,14 +59,71 @@ public class InventoryController {
     @FXML
     private TextField reorderThreshold1;
 
+    //Connection to Database
+    private static final HikariDataSource ds;
+    static {
+        Dotenv dotenv = Dotenv.load();
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://" +
+                dotenv.get("DB_HOST") + ":5432/" +
+                dotenv.get("DB_NAME"));
+        config.setUsername(dotenv.get("DB_USER"));
+        config.setPassword(dotenv.get("DB_PASSWORD"));
+        config.setMaximumPoolSize(10);
+        ds = new HikariDataSource(config);
+    }
+    public static Connection getConnection() throws SQLException {
+        return ds.getConnection();
+    }
+
+        //Builds Alerts
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
     @FXML
     void addInventoryItem(ActionEvent event) {
+        // Complete
+        String name = itemName.getText();
+        String ID = itemID.getText();
+        String quantity = itemQuantity.getText();
+        String price = itemPrice.getText();
+        String type = ingredientType.getText();
+        String threshold = reorderThreshold.getText();
 
+        String sql = "INSERT INTO public.\"Inventory\"(\"inventoryId\",\"name\",\"quantityOnHand\",\"reorderThreshold\",\"pricePerUnit\",\"type\") VALUES (?,?,?,?,?,?);";
+         try (Connection conn = getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setInt(1, Integer.parseInt(ID));
+            stmt.setString(2, name);
+            stmt.setInt(3, Integer.parseInt(quantity));
+            stmt.setDouble(5, Double.parseDouble(price));
+            stmt.setInt(4, Integer.parseInt(threshold));
+            stmt.setString(6, type);
+            stmt.executeUpdate();
+            
+            itemName.clear();
+            itemID.clear();
+            itemQuantity.clear();
+            itemPrice.clear();
+            ingredientType.clear();
+            reorderThreshold.clear();
+
+            // loadEmployees();
+            showAlert(Alert.AlertType.INFORMATION, "Success", "Ingredient added successfully!");
+        } catch (SQLException ex){
+            ex.printStackTrace();
+            showAlert(Alert.AlertType.ERROR, "Data Base Error", ex.getMessage());
+        }
     }
 
     @FXML
     void editInventoryItem(ActionEvent event) {
-
+        //TODO
     }
 
     @FXML
