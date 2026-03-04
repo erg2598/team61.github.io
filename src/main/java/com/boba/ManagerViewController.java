f4cf7446cce6bc11e36b5e63586fe9b441cff967
Grant Duong <grantd729@gmail.com>
2026-03-04 10:58:29 -0600

    In class activities

--- src/main/java/com/boba/ManagerViewController.java
+++ src/main/java/com/boba/ManagerViewController.java
@@ -4,11 +4,19 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.stage.Stage;
 import javafx.scene.control.Button;
+/**
+ * @author Grant Duong, Nilay Alwar, Eli Goodrich, Maher Zaveri, Jack Anderson
+*/
 
 public class ManagerViewController {
 
     @FXML private Button launchEmployeeBtn; // wire any button for stage reference
 
+    /**
+     * @param ActionEvent This function will trigger when a button is clicked.
+     * @return Returns nothing.
+     * @throws none there is no error handling for this code
+     */
     @FXML
     void launchEmployeeView(ActionEvent event) {
         Stage stage = (Stage) ((javafx.scene.Node) event.getSource()).getScene().getWindow();
